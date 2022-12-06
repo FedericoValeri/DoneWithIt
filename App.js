f9f2bdd2ff04aa892f952f4bd833c9eb0688c96b
Federico <federico.valeri@unicam.it>
2022-12-06 17:39:30 +0100

    ViewImageScreen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
