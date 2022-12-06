faba60fab8d15fd4bbdcc036012e21649fc4dc8a
Federico <federico.valeri@unicam.it>
2022-12-06 17:55:58 +0100

    colors file

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,7 @@
-import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+import colors from "../config/colors";
 
 export default function ViewImageScreen() {
   return (
@@ -19,19 +21,19 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   container: {
-    backgroundColor: "#000",
+    backgroundColor: colors.black,
     flex: 1,
   },
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
