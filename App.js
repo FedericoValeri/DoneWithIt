70299cab409ba4d651be98ea55e0f82669069f6c
Federico <federico.valeri@unicam.it>
2022-12-07 13:03:23 +0100

    Card component

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
+import { View } from "react-native";
+import Card from "./app/components/Card";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  //return <WelcomeScreen />;
+  return (
+    <View
+      style={{ backgroundColor: "lightgrey", padding: 20, paddingTop: 100 }}
+    >
+      <Card
+        title={"Red jacket for sale"}
+        subTitle="€100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
