3295a8163543b8631a995267f6e90225ae640e98
Federico <federico.valeri@unicam.it>
2022-12-07 13:34:27 +0100

    ListItem component

---
+++ app/components/ListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+import colors from "../config/colors";
+import AppText from "./AppText";
+
+export default function ListItem({ title, subTitle, image }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "700",
+  },
+});
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, StyleSheet, View } from "react-native";
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 import colors from "../config/colors";
 
 export default function ListingDetailsScreen() {
@@ -10,6 +11,13 @@ export default function ListingDetailsScreen() {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>€100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mario-rossi.jpg")}
+            title="Mario Rossi"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -32,5 +40,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
