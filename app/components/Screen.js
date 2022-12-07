e0d432d9c84a336238cbca164ef3fac7705c5196
Federico <federico.valeri@unicam.it>
2022-12-07 16:23:52 +0100

    Screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,13 @@
+import Constants from "expo-constants";
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+
+export default function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
 import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
 
 const messages = [
   {
@@ -19,17 +20,19 @@ const messages = [
 
 export default function MessagesScreen() {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-        />
-      )}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   );
 }
 
