b2d5ba6c8415e1ec78c8bc6976fd3cc49f031933
Federico <federico.valeri@unicam.it>
2022-12-07 14:24:23 +0100

    MaterialCommunityIcons

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { View } from "react-native";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  //return <WelcomeScreen />;
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
