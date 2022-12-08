f4d1e4345d9bc954018d0585abda92bb25961a40
Federico <federico.valeri@unicam.it>
2022-12-08 13:28:50 +0100

    Icon component

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { View } from "react-native";
 import Card from "./app/components/Card";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" />
+    </Screen>
+  );
 }
