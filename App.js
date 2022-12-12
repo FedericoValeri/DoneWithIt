a718160fdd8033cc218b88f77154ea9100351f99
Federico <federico.valeri@unicam.it>
2022-12-12 20:44:20 +0100

    Login Form

--- App.js
+++ App.js
@@ -13,30 +13,12 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import { useState } from "react";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      {
-        <Screen>
-          <AppPicker
-            selectedItem={category}
-            onSelectItem={(item) => setCategory(item)}
-            items={categories}
-            placeholder={"Category"}
-            icon="apps"
-          />
-          <AppTextInput placeholder="Email" icon="email" />
-        </Screen>
-      }
+      {<LoginScreen />}
     </GestureHandlerRootView>
   );
 }
