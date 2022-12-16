29c05876a09eb2ffa0400f610650168a9a292655
Federico <federico.valeri@unicam.it>
2022-12-16 16:29:13 +0100

    Long text handling

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Text } from "react-native";
 import defaultStyles from "../config/styles";
 
-export default function AppText({ children, style }) {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+export default function AppText({ children, style, ...otherProps }) {
+  return (
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 }
--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -20,8 +20,14 @@ export default function ListItem({
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText style={styles.title} numberOfLines={1}>
+              {title}
+            </AppText>
+            {subTitle && (
+              <AppText style={styles.subTitle} numberOfLines={2}>
+                {subTitle}
+              </AppText>
+            )}
           </View>
           <MaterialCommunityIcons
             color={colors.medium}
