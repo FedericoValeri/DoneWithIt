faa66aece630a95dbe0045a19fb352cc5f3cf6c3
Federico <federico.valeri@unicam.it>
2022-12-29 15:02:58 +0100

    ImageInput touches

--- App.js
+++ App.js
@@ -39,9 +39,10 @@ export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Screen>
-        <Button title="Select image" onPress={selectImage} />
-        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-        <ImageInput imageUri={imageUri} />
+        <ImageInput
+          imageUri={imageUri}
+          onChangeImage={(uri) => setImageUri(uri)}
+        />
       </Screen>
     </GestureHandlerRootView>
   );
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,15 +1,64 @@
-import { Image, StyleSheet, View } from "react-native";
-import colors from "../config/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import colors from "../config/colors";
 
-export default function ImageInput({ imageUri }) {
+export default function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      alert("Yuo need to enable permission to access Gallery");
+    }
+  };
+
+  const handlePress = () => {
+    if (!imageUri) {
+      selectImage();
+    } else {
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        {
+          text: "Yes",
+          onPress: () => onChangeImage(null),
+        },
+        { text: "No" },
+      ]);
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.canceled) {
+        onChangeImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image.", error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons color={colors.medium} name="camera" />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons color={colors.medium} name="camera" />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
