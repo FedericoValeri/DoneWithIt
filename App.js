430d7d4abb8aa8f1a2f09f6044afe7d27acd28e5
Federico <federico.valeri@unicam.it>
2022-12-30 11:40:56 +0100

    Stack Navigator

--- App.js
+++ App.js
@@ -7,14 +7,38 @@ import LoginScreen from "./app/screens/LoginScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import Screen from "./app/components/Screen";
 import ImageInputList from "./app/components/ImageInputList";
-import { Button, Image } from "react-native";
+import { Button, Image, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useEffect, useState } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="Tweet Details" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingEditScreen />
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
