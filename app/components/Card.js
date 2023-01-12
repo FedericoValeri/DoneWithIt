ea06c3faf1631e43f857c2600fe76658e61ddcd2
Federico <federico.valeri@unicam.it>
2023-01-12 16:34:41 +0100

    Fetch data from server with apisauce

--- app/components/Card.js
+++ app/components/Card.js
@@ -9,11 +9,11 @@ import {
 import AppText from "./AppText";
 import colors from "../config/colors";
 
-export default function Card({ title, subTitle, image, onPress }) {
+export default function Card({ title, subTitle, imageUrl, onPress }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={image} />
+        <Image style={styles.image} source={{ uri: imageUrl }} />
         <View style={styles.detailsContainer}>
           <AppText style={styles.title}>{title}</AppText>
           <AppText style={styles.subTitle}>{subTitle}</AppText>
