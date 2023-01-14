0e9f8dc12507ae70e942066525e991398ea42d6c
Federico <federico.valeri@unicam.it>
2023-01-14 10:10:01 +0100

    Form reset

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -5,12 +5,14 @@ import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
 export default function AppFormField({ name, width, ...otherProps }) {
-  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+  const { setFieldTouched, setFieldValue, errors, touched, values } =
+    useFormikContext();
   return (
     <>
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={(text) => setFieldValue(name, text)}
+        value={values[name]}
         width={width}
         {...otherProps}
       />
